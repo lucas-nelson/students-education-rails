abe8dab3def0b4dd60f979e5fc5e0d4df3d26e7b
Lucas Nelson <lucas-nelson@users.noreply.github.com>
2016-06-15 08:08:28 +1000

    Better test coverage on teachers#show

--- spec/views/teachers/show.html.erb_spec.rb
+++ spec/views/teachers/show.html.erb_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-# rubocop:disable Lint/AmbiguousRegexpLiteral
 RSpec.describe 'teachers/show.html.erb', type: :view do
   it 'shows a teacher' do
     assign(:teacher, FactoryGirl.create(:teacher, email: 'teacher_one@example.com', name: 'Teacher One'))
 
     render
 
-    expect(rendered).to match /teacher_one@example.com/
-    expect(rendered).to match /Teacher One/
+    expect(rendered).to have_css 'h2', text: 'Teacher One'
+    expect(rendered).to have_css 'dd', text: 'teacher_one@example.com'
+    expect(rendered).to have_link 'Students taught'
   end
 end
