8a9698fb73f14b11864ad90d85fce19a2922cf94
Lucas Nelson <lucas-nelson@users.noreply.github.com>
2016-06-15 08:05:14 +1000

    Better test coverage on students#show

--- spec/views/students/show.html.erb_spec.rb
+++ spec/views/students/show.html.erb_spec.rb
@@ -1,13 +1,25 @@
 require 'rails_helper'
 
-# rubocop:disable Lint/AmbiguousRegexpLiteral
 RSpec.describe 'students/show.html.erb', type: :view do
   it 'shows a student' do
     assign(:student, FactoryGirl.create(:student, email: 'student_one@example.com', name: 'Student One'))
 
     render
 
-    expect(rendered).to match /student_one@example.com/
-    expect(rendered).to match /Student One/
+    expect(rendered).to have_css 'h2', text: 'Student One'
+    expect(rendered).to have_css 'dd', text: 'student_one@example.com'
+    expect(rendered).to have_css 'p', text: 'No progress'
+    expect(rendered).to have_link 'Set progress'
+  end
+
+  it 'shows a student with progress' do
+    assign(:student, FactoryGirl.create(:student,
+                                        :with_lesson_parts,
+                                        email: 'student_one@example.com',
+                                        name: 'Student One'))
+
+    render
+
+    expect(rendered).to have_css 'p', text: 'Completed up to lesson 2, part 3'
   end
 end
