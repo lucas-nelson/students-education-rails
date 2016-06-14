f10d92fdd9b9b6fa33fef034f59d0fe0fdf647af
Lucas Nelson <lucas-nelson@users.noreply.github.com>
2016-06-15 08:37:53 +1000

    Support a JSON representation of a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,9 +8,26 @@ class StudentsController < ApplicationController
   end
 
   def show
+    respond_to do |format|
+      format.html {}
+      format.json { render json: hash_for_show_json(@student) }
+    end
   end
 
   private
+
+    def hash_for_progress(lesson_part)
+      return {} unless lesson_part
+
+      { lesson: lesson_part.lesson.ordinal, part: lesson_part.ordinal }
+    end
+
+    def hash_for_show_json(student)
+      last_lesson_part = student.lesson_parts&.last
+      progress = hash_for_progress(last_lesson_part)
+
+      { student: @student, progress: progress }
+    end
 
     def set_student
       @student = Student.find(params[:id])
